b734ea88f55655a5f69b00dcc0435843d3a5cdc1
Margarita <kulachenkova@gmail.com>
2024-09-16 11:41:46 +0300

    upd

--- insight_stream/client.py
+++ insight_stream/client.py
@@ -14,13 +14,17 @@ import requests
 import urllib.parse
 import socket
 
-OPENAI_BASE = os.getenv('OPENAI_BASE')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-EMBED_MODEL = os.getenv('EMBED_MODEL')
+EMBED_MODEL_URL = os.getenv('EMBED_MODEL_URL')
+EMBED_MODEL_API_KEY = os.getenv('EMBED_MODEL_API_KEY')
+EMBED_MODEL_NAME = os.getenv('EMBED_MODEL_NAME')
+
 ENCODING_FORMAT = os.getenv('ENCODING_FORMAT')
 TIKTOKEN_MODEL = os.getenv('TIKTOKEN_MODEL')
 TIKTOKEN_ENABLED = bool(os.getenv('TIKTOKEN_ENABLED'))
-CHAT_MODEL = os.getenv('CHAT_MODEL')
+
+CHAT_MODEL_URL = os.getenv('CHAT_MODEL_URL')
+CHAT_MODEL_API_KEY = os.getenv('CHAT_MODEL_API_KEY')
+CHAT_MODEL_NAME = os.getenv('CHAT_MODEL_NAME')
 
 QDRANT_URL = os.getenv('QDRANT_URL')
 QDRANT_KEY = os.getenv('QDRANT_KEY')
@@ -29,43 +33,21 @@ QDRANT_VECTOR_SIZE = int(os.getenv('QDRANT_VECTOR_SIZE', 1536))
 SERVER_NAME = os.getenv('SERVER_NAME')
 TOKEN = os.getenv('TOKEN')
 
-embeddings = OpenAIEmbeddings(model=EMBED_MODEL,
-                              openai_api_base=OPENAI_BASE,
-                              openai_api_key=OPENAI_API_KEY,
+embeddings = OpenAIEmbeddings(model=EMBED_MODEL_NAME,
+                              openai_api_base=EMBED_MODEL_URL,
+                              openai_api_key=EMBED_MODEL_API_KEY,
                               model_kwargs={"encoding_format": ENCODING_FORMAT},
                               tiktoken_enabled=TIKTOKEN_ENABLED,
                               tiktoken_model_name=TIKTOKEN_MODEL,
                               )
 
-llmLama = ChatOpenAI(
-    openai_api_key=OPENAI_API_KEY,
-    model=CHAT_MODEL,
+llm = ChatOpenAI(
+    openai_api_key=CHAT_MODEL_API_KEY,
+    model=CHAT_MODEL_NAME,
     temperature=0,
-    openai_api_base=OPENAI_BASE
-)
-
-llm2 = ChatOpenAI(
-    openai_api_key='sk-...',
-    model="gpt-4o-mini",
-    temperature=0,
-    openai_api_base="https://ep.redsquad.tech/openai/"
-)
-
-
-llm_Qwen = ChatOpenAI(
-    openai_api_key='qwen_key_here',
-    model="/app/resources/models/models/Qwen_Qwen2-7B-Instruct",
-    temperature=0,
-    openai_api_base="https://api.qdrant.mil-team.ru/chat-1/v1/",
-	request_timeout=120,  # Установите таймаут в 120 секунд
+    openai_api_base=CHAT_MODEL_URL,
+	request_timeout=120,
     max_retries=5  
-)
-
-llm_Qwen_local = ChatOpenAI(
-    openai_api_key='key',
-    model="qwen2",
-    temperature=0,
-    openai_api_base="http://127.0.0.1:11434/v1/",
 )
 
 clientQdrant=QdrantClient(url=QDRANT_URL, api_key=QDRANT_KEY)
@@ -101,43 +83,21 @@ def _add_documents(index_id: str, documents: List[Document], file_path: str) -> 
 
 	docs_for_qdrant = []
 
-	prompt_kw = ChatPromptTemplate.from_template("Выбери абревиатуры из текста {chunk} для которых есть расшифровка значения, в виде списка, разделенного запятыми. Не выделяй абревиатуры ковычками. Абревиатуры типа ст., ч., п., РФ и стандартные сокращения (г., ..) не нужны, только специфические для данного стандарта. В ответ дай только сам список абревиатур. Используй русский язык, не используй вступительные фразы а-ля 'Вот список ключевых слов и абревиатур' или 'Аббревиатуры:' Давай в ответ сами абревиатуры а не их номера.")
-	prompt_qa = ChatPromptTemplate.from_template("Сгенерируй основные вопросы, которые лучше отражают содержание текста {chunk} (учти, что текст это часть документа {name}). Дай только сам список вопросов на русском языке, разделяй вопросы с помощью символа переноса строки, нумеровать вопросы не нужно. убедись, что вопросы не повторяются. если ссылаешься внутри вопроса на приказ или политику, указывай его номер. не используй вступительные фразы а-ля 'Вот основные вопросы на которые отвечает текст'.")
-# Затем Выбери те абревиатуры из этого же текста для которых есть расшифровка значения в виде списка, разделенного запятыми. Нужно не более 10 абревиатур. абревиатуры типа ст., ч., п., г., РФ не нужны, только специфические для данного стандарта. В ответ дай только сам список абревиатур. Используй русский язык, не используй вступительные фразы а-ля 'Вот список абревиатур' или 'Аббревиатуры:'. Отдели абервиатуры от вопросов с помошью ###
-
-
-	prompt_qa2 = ChatPromptTemplate.from_template("""Сгенерируй основные вопросы, которые лучше отражают содержание текста {chunk} (учти, что текст это часть документа {name}). 
-												Если текст текст не отвечает ни на какие вопросы скажи 'тут нет вопросов'. 
-												Дай только сам список вопросов на русском языке, разделяй вопросы с помощью символа переноса строки, нумеровать вопросы не нужно. 
-												если ссылаешься внутри вопроса на приказ или политику, указывай его номер. 
-												не используй вступительные фразы а-ля 'Вот 3-5 вопросов на которые отвечает текст'.
-												Затем Выбери те абревиатуры из этого же текста для которых есть расшифровка значения в виде списка, разделенного запятыми. 
-												Абревиатуры типа ст., ч., п., РФ и стандартные сокращения (г., ..) не нужны, только специфические для данного стандарта. 
-												В ответ дай только сам список абревиатур. Используй русский язык, не используй вступительные фразы а-ля 'Вот список абревиатур' или 'Аббревиатуры:'. 
-												Отдели абервиатуры от вопросов с помошью ### (например Воспрос1?\nВопрос2?\n... ### КПСС, ЛДПР, ...)""")
-
-	chain_kw = prompt_kw | llm_Qwen_local | StrOutputParser()
-	chain_qa = prompt_qa | llm_Qwen_local | StrOutputParser()
+	prompt_kw = ChatPromptTemplate.from_template("Выбери абревиатуры из текста {chunk} для которых есть расшифровка значения, в виде списка, разделенного запятыми. Каждая абревиатура нужна только один раз без повторений. Не выделяй абревиатуры ковычками. Абревиатуры типа ст., ч., п., РФ и стандартные сокращения (г., ..) не нужны, только специфические для данного стандарта. В ответ дай только список уникальных абревиатур. Используй русский язык и именительный падеж, не используй вступительные фразы а-ля 'Вот список ключевых слов и абревиатур' или 'Аббревиатуры:' Давай в ответ сами абревиатуры а не их номера. Не давай никаких примечаний")
+	prompt_qa = ChatPromptTemplate.from_template("Сгенерируй основные вопросы, которые лучше отражают содержание текста {chunk} (учти, что текст это часть документа {name}). Дай только сам список вопросов на русском языке, разделяй вопросы с помощью символа переноса строки, нумеровать вопросы не нужно, учти, что далее по данным вопросам будет производится поиск нужного текса, следовательно вопросы должны передавать специфику текста. убедись, что вопросы не повторяются. если ссылаешься внутри вопроса на приказ или политику, указывай его номер. не используй вступительные фразы а-ля 'Вот основные вопросы на которые отвечает текст'.")
+	
+	chain_kw = prompt_kw | llm | StrOutputParser()
+	chain_qa = prompt_qa | llm | StrOutputParser()
 
 	for doc in documents:
 
 		keywords = chain_kw.invoke({"chunk": doc.page_content})
-		#print(keywords, "\n*****")
 		print("keywords:")
 		print(keywords)
 
 		questions = chain_qa.invoke({"chunk": doc.page_content, "name": os.path.splitext(os.path.basename(file_path))[0]})
-		#print(questions1, "\n********************")
-
-		#res = questions1.split("###")
-
-		#questions = res[0].replace("\n\n", "\n").strip().replace("тут нет вопросов", '')
-		#keywords  = res[1].strip()
-
 		print("questions:")
 		print(questions)
-
-
 
 
 		new_doc = Document(page_content=keywords.replace('"', '') + "\n" + questions, metadata = {
@@ -146,7 +106,7 @@ def _add_documents(index_id: str, documents: List[Document], file_path: str) -> 
 			"extension": os.path.splitext(os.path.basename(file_path))[1][1:].lower(),
 			"keywords": keywords,
 			"questions": questions,
-			"page_content": doc.page_content}, #"page_number": doc.metadata.page_number
+			"page_content": doc.page_content},
 			)
 		docs_for_qdrant.append(new_doc)
 		
@@ -191,7 +151,7 @@ def upload_doc(index_id: str, path: str) -> List[Document]:
 	for chunk in chunks:
 		text = str(chunk)
  
-		if len(cur_text + text) <= 10400:
+		if len(cur_text + text) <= 10000:
 			cur_text += '\n' + text if cur_text else text
 		else:
 			print(len(cur_text))
@@ -208,7 +168,7 @@ def upload_doc(index_id: str, path: str) -> List[Document]:
 	#загрузка чанков документа в квадрант
 	for chunk in final_chunks:
 		try:
-			docs = _add_documents(index_id, [chunk], path) #final_chunks
+			docs = _add_documents(index_id, [chunk], path)
 		except Exception as err:
 			print(err)
 			docs = _add_documents(index_id, [chunk], path)
