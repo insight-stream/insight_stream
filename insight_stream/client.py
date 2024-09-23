8294b73463e95fed61b7e1eeefa33d3678bcce8c
Gleb Morgachev <morgachev.g@gmail.com>
2024-09-23 10:28:26 -0700

    Update client

--- insight_stream/client.py
+++ insight_stream/client.py
@@ -1,6 +1,8 @@
 from typing import Dict, List, Optional
 import json
 import os
+import logging
+
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_qdrant import Qdrant
 from qdrant_client import QdrantClient, models
@@ -20,7 +22,6 @@ EMBED_MODEL_NAME = os.getenv('EMBED_MODEL_NAME')
 
 ENCODING_FORMAT = os.getenv('ENCODING_FORMAT')
 TIKTOKEN_MODEL = os.getenv('TIKTOKEN_MODEL')
-TIKTOKEN_ENABLED = bool(os.getenv('TIKTOKEN_ENABLED'))
 
 CHAT_MODEL_URL = os.getenv('CHAT_MODEL_URL')
 CHAT_MODEL_API_KEY = os.getenv('CHAT_MODEL_API_KEY')
@@ -33,30 +34,43 @@ QDRANT_VECTOR_SIZE = int(os.getenv('QDRANT_VECTOR_SIZE', 1536))
 SERVER_NAME = os.getenv('SERVER_NAME')
 TOKEN = os.getenv('TOKEN')
 
-embeddings = OpenAIEmbeddings(model=EMBED_MODEL_NAME,
-                              openai_api_base=EMBED_MODEL_URL,
-                              openai_api_key=EMBED_MODEL_API_KEY,
-                              model_kwargs={"encoding_format": ENCODING_FORMAT},
-                              tiktoken_enabled=TIKTOKEN_ENABLED,
-                              tiktoken_model_name=TIKTOKEN_MODEL,
-                              )
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+embeddings = OpenAIEmbeddings(
+	model=EMBED_MODEL_NAME,
+	openai_api_base=EMBED_MODEL_URL,
+	openai_api_key=EMBED_MODEL_API_KEY,
+	model_kwargs={"encoding_format": ENCODING_FORMAT},
+	tiktoken_enabled=False,
+	tiktoken_model_name=TIKTOKEN_MODEL,
+)
 
 llm = ChatOpenAI(
     openai_api_key=CHAT_MODEL_API_KEY,
     model=CHAT_MODEL_NAME,
     temperature=0,
     openai_api_base=CHAT_MODEL_URL,
-	request_timeout=120,
-    max_retries=5  
+	request_timeout=300,
+    max_retries=3,
+	model_kwargs={
+		"max_tokens": 600,
+		"repetition_penalty": 3.0,
+		"frequency_penalty": 2.0,
+	}
 )
 
 clientQdrant=QdrantClient(url=QDRANT_URL, api_key=QDRANT_KEY)
+
 
 def ask(
 		bot_id: str,
 		question: str
 	) -> Optional[Dict[str, any]]:
 	""""Получение ответа бота на вопрос пользователя"""
+	logger.info(f"Request to bot {bot_id}: {question}")
 	request = requests.post(
 		f'{SERVER_NAME}/v.1.0/{bot_id}',
 		data=json.dumps({ "question": question }),
@@ -66,54 +80,80 @@ def ask(
 		}
 	)
 	if request.status_code == 200:
-		return request.json()
+		response = request.json()
+		logger.info(f"Response from bot {bot_id}: {response}")
+		return response
 	else:
+		logger.error(f"Error in bot {bot_id}: {request.status_code} {request.text}")
 		return None
 	
 def _add_documents(index_id: str, documents: List[Document], file_path: str) -> List[Document]:
 	"""Добавляем в квадрант документы, являющиеся чанками для одного файла из папки"""
 	try:
 		clientQdrant.create_collection(
-				collection_name=index_id,
-				vectors_config=models.VectorParams(size=QDRANT_VECTOR_SIZE, distance=models.Distance.COSINE)
-			)
-	except:
+			collection_name=index_id,
+			vectors_config=models.VectorParams(size=QDRANT_VECTOR_SIZE, distance=models.Distance.COSINE)
+		)
+	except Exception as e:
+		logger.debug(f"Error in creating collection {index_id}: {e}")
 		pass
+
 	qdrant = Qdrant(client=clientQdrant, collection_name=index_id, embeddings=embeddings)
 
 	docs_for_qdrant = []
 
-	prompt_kw = ChatPromptTemplate.from_template("Выбери абревиатуры из текста {chunk} для которых есть расшифровка значения, в виде списка, разделенного запятыми. Каждая абревиатура нужна только один раз без повторений. Не выделяй абревиатуры ковычками. Абревиатуры типа ст., ч., п., РФ и стандартные сокращения (г., ..) не нужны, только специфические для данного стандарта. В ответ дай только список уникальных абревиатур. Используй русский язык и именительный падеж, не используй вступительные фразы а-ля 'Вот список ключевых слов и абревиатур' или 'Аббревиатуры:' Давай в ответ сами абревиатуры а не их номера. Не давай никаких примечаний")
-	prompt_qa = ChatPromptTemplate.from_template("Сгенерируй основные вопросы, которые лучше отражают содержание текста {chunk} (учти, что текст это часть документа {name}). Дай только сам список вопросов на русском языке, разделяй вопросы с помощью символа переноса строки, нумеровать вопросы не нужно, учти, что далее по данным вопросам будет производится поиск нужного текса, следовательно вопросы должны передавать специфику текста. убедись, что вопросы не повторяются. если ссылаешься внутри вопроса на приказ или политику, указывай его номер. не используй вступительные фразы а-ля 'Вот основные вопросы на которые отвечает текст'.")
+	prompt_kw = ChatPromptTemplate.from_template(
+		"Выбери уникальные абревиатуры из текста {chunk} для которых есть расшифровка значения, " + \
+		"в виде списка, разделенного запятыми. Каждая абревиатура нужна только один раз без повторений. " + \
+		"Не выделяй абревиатуры ковычками. Абревиатуры типа ст., ч., п., РФ и стандартные сокращения (г., ..) не нужны," + \
+		"только специфические для данного стандарта. В ответ дай только список уникальных абревиатур. " + \
+		"Используй русский язык и именительный падеж, не используй вступительные фразы а-ля 'Вот список ключевых слов и абревиатур' " + \
+		"или 'Аббревиатуры:' Давай в ответ сами абревиатуры а не их номера. Не давай никаких примечаний"
+	)
+	prompt_qa = ChatPromptTemplate.from_template(
+		"Сгенерируй уникальные основные вопросы, которые лучше отражают содержание текста {chunk} " + \
+		"(учти, что текст это часть документа {name}). Дай только сам список вопросов на русском языке, " + \
+		"разделяй вопросы с помощью символа переноса строки, нумеровать вопросы не нужно, учти, " + \
+		"что далее по данным вопросам будет производится поиск нужного текса, " + \
+		"следовательно вопросы должны передавать специфику текста. убедись, что вопросы не повторяются. " + \
+		"если ссылаешься внутри вопроса на приказ или политику, указывай его номер. " \+
+		"не используй вступительные фразы а-ля 'Вот основные вопросы на которые отвечает текст'."
+	)
 	
 	chain_kw = prompt_kw | llm | StrOutputParser()
 	chain_qa = prompt_qa | llm | StrOutputParser()
 
-	for doc in documents:
 
+	for idx, doc in enumerate(documents):
+		logger.info(f"Processing document: {idx}")
+
+		logger.info("Generating keywords")
 		keywords = chain_kw.invoke({"chunk": doc.page_content})
-		print("keywords:")
-		print(keywords)
+		logger.debug(f"Keywords: {keywords}")
 
+		logger.info("Generating questions")
 		questions = chain_qa.invoke({"chunk": doc.page_content, "name": os.path.splitext(os.path.basename(file_path))[0]})
-		print("questions:")
-		print(questions)
+		logger.debug(f"Questions: {questions}")
 
 
-		new_doc = Document(page_content=keywords.replace('"', '') + "\n" + questions, metadata = {
-			"topic": os.path.splitext(os.path.basename(file_path))[0], 
-			"url": urllib.parse.quote(f'{SERVER_NAME}/documents/{os.path.basename(file_path)}', safe=":/"),
-			"extension": os.path.splitext(os.path.basename(file_path))[1][1:].lower(),
-			"keywords": keywords,
-			"questions": questions,
-			"page_content": doc.page_content},
-			)
+		new_doc = Document(
+			page_content=keywords.replace('"', '') + "\n" + questions,
+			metadata = {
+				"topic": os.path.splitext(os.path.basename(file_path))[0], 
+				"url": urllib.parse.quote(f'{SERVER_NAME}/documents/{os.path.basename(file_path)}', safe=":/"),
+				"extension": os.path.splitext(os.path.basename(file_path))[1][1:].lower(),
+				"keywords": keywords,
+				"questions": questions,
+				"page_content": doc.page_content
+			},
+		)
 		docs_for_qdrant.append(new_doc)
-		
+
 	try:
+		logger.info("Adding documents to Qdrant")
 		qdrant.add_documents(docs_for_qdrant)
 	except Exception as e:
-		print(e)
+		logger.error(f"Error in adding documents to Qdrant: {e}")
 	return docs_for_qdrant
 
 
@@ -170,7 +210,7 @@ def upload_doc(index_id: str, path: str) -> List[Document]:
 		try:
 			docs = _add_documents(index_id, [chunk], path)
 		except Exception as err:
-			print(err)
+			logger.error(f"Error in adding document {index_id} to Qdrant: {err}")
 			docs = _add_documents(index_id, [chunk], path)
 
 	
@@ -180,6 +220,7 @@ def upload_doc(index_id: str, path: str) -> List[Document]:
 	url = _load_file_to_server(path)
 	
 	return docs
+
 
 def upload_dir(index_id: str, path: str) -> List[Document]:
 	"""Множественная загрузка документов в индекс квадранта и на сервер"""
@@ -234,7 +275,8 @@ def _load_file_to_server(file_path: str) -> str:
 	else:
 		print(f"Загрузка файла {os.path.basename(file_path)} на сервер не удалась")
 		return ''
-	
+
+
 def _del_file_from_server(url: str):
 	"""Удаление файла с сервера"""
 
